3c191185894ccce252e8e2ccb173809c263f3453
Moesio Marcelino <moesiomarcelino1@gmail.com>
2023-03-27 14:04:34 -0300

    feat: add pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { globalStyles } from '@/styles/globals'
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from "../assets/logo.svg"
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
+
+export default App
